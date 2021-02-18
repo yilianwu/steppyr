cc047898948bdd841db453895fae5f14697f9e3b
Leo <leo881003@gmail.com>
2021-02-18 16:05:26 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,64 @@
-from setuptools import setup, find_packages
+import os
+import sys
+import subprocess
+
+from setuptools import setup, find_packages, Extension
+from setuptools.command.build_ext import build_ext
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=""):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+class CMakeBuild(build_ext):
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+
+        # required for auto-detection of auxiliary "native" libs
+        if not extdir.endswith(os.path.sep):
+            extdir += os.path.sep
+
+        # CMake lets you override the generator - we need to check this.
+        # Can be set with Conda-Build, for example.
+        cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
+
+        # Set Python_EXECUTABLE instead if you use PYBIND11_FINDPYTHON
+        # EXAMPLE_VERSION_INFO shows you how to pass a value into the C++ code
+        # from Python.
+        cfg = "Debug" if self.debug else "Release"
+        cmake_args = [
+            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={}".format(extdir),
+            "-DPYTHON_EXECUTABLE={}".format(sys.executable),
+            "-DEXAMPLE_VERSION_INFO={}".format(self.distribution.get_version()),
+            "-DCMAKE_BUILD_TYPE={}".format(cfg),  # not used on MSVC, but no harm
+        ]
+        build_args = []
+
+        if not cmake_generator:
+            cmake_args += ["-GNinja"]
+
+        # Set CMAKE_BUILD_PARALLEL_LEVEL to control the parallel build level
+        # across all generators.
+        if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
+            # self.parallel is a Python 3 only way to set parallel jobs by hand
+            # using -j in the build_ext call, not supported by pip or PyPA-build.
+            if hasattr(self, "parallel") and self.parallel:
+                # CMake 3.12+ only.
+                build_args += ["-j{}".format(self.parallel)]
+
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+
+        subprocess.check_call(
+            ["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp
+        )
+        subprocess.check_call(
+            ["cmake", "--build", "."] + build_args, cwd=self.build_temp
+        )
 
 setup(
     name="steppyr",
-    version="1.0.1",
+    version="1.1.0",
     author="Alan Gibson",
     author_email="alan.gibson@gmail.com",
     description="A stepper motor library in Python",
@@ -10,11 +66,10 @@ setup(
     long_description_content_type="text/markdown",
     keywords="servo motor library raspberry pi stepper",
     url="https://github.com/alangibson/steppyr",
-    packages=find_packages(where='src'),
-    package_dir={"": "src"},
     data_files=[('', ['README.md'])],
-    install_requires=['RPi.GPIO', 'spidev'],
     setup_requires=['pip'],
+    ext_modules=[CMakeExtension("steppyr")],
+    cmdclass={"build_ext": CMakeBuild},
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
